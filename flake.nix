6cf499a7d692140ca987cf34988f8a4e58b3cf21
roo7gb <miasma1007@gmail.com>
2026-07-15 18:13:19 -0400

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,19 @@
+# nix-wrapper-overlays/flake.nix
+
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self }: {
+
+    overlays = {
+      fastfetch = import ./overlays/fastfetch;
+      hyprland = import ./overlays/hyprland;
+      hyprlock = import ./overlays/hyprlock;
+      nushell = import ./overlays/nushell;
+      wezterm = import ./overlays/wezterm;
+    };
+  };
+}
+
