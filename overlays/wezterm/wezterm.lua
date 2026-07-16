6dd8da8438006c8fef42b665f2261f6e47514c91
roo7gb <miasma1007@gmail.com>
2026-07-15 21:47:25 -0400

    Force no bold

--- overlays/wezterm/wezterm.lua
+++ overlays/wezterm/wezterm.lua
@@ -6,6 +6,7 @@ local config = wezterm.config_builder and wezterm.config_builder() or {}
 -- font config
 config.font = wezterm.font 'Mononoki Nerd Font'
 config.font_size = 14.0
+config.font_rules.weight = 'Regular'
 
 config.freetype_load_target = 'Light'
 config.freetype_render_target = 'Light'
